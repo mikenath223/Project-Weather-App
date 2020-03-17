fb6f4354e5ce77043973fee9e7004401878ff195
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-03-18 00:56:52 +0100

    [Fix] Further fix to lint errors

--- webpack.config.js
+++ webpack.config.js
@@ -32,11 +32,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader'],
       },
     ],
